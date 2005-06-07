4c93e75bbf6ccc7e866d487295a08762955f25c3
Robert Widhopf <hack@robf.de>
2005-06-07 05:58:05 +0000

    indention bugfixes, lookup webdocs and comment region - fixed some indetion problems, e.g. (...), Return, ... - added lookup of web docs for command at point using w3 - improved (un)commenting - added a menu

--- ahk-mode.el
+++ ahk-mode.el
@@ -29,18 +29,14 @@
 ;;
 ;; Place this file somewhere in your load-path, byte-compile it and add the
 ;; following line to your ~/.xemacs/init.el resp. ~/.emacs:
-;;
-;; (setq ahk-syntax-directory "PATHTO/AutoHotkey/Extras/Editors/Syntax/")
-;; (add-to-list 'auto-mode-alist '("\\.ahk$" . ahk-mode))
-;; (autoload 'ahk-mode "ahk-mode")
+;;   (require 'ahk-mode)
 ;; 
 ;; The first time ahk-mode.el is started it will ask you for the path to the
-;; Syntax directory if not set already.  You will find it as a subdirectory
-;; of your AHK installation.
+;; Syntax directory which you will find in the subdirectory of your AHK
+;; installation.
 ;;
 ;; For example if you installed AHK at C:\Programms\AutoHotKey it will be
-;; C:/Programms/AutoHotKey/Extras/Editors/Syntax or a corresponding cygwin
-;; path!
+;; C:/Programms/AutoHotKey/Extras/Editors/Syntax !
 ;;
 ;; When opening a script file you will get:
 ;; - syntax highlighting
@@ -48,14 +44,17 @@
 ;; - insertion of command templates (bound to "C-c C-i") 
 ;; - electric braces (typing "{" will also insert "}" and place point in
 ;;   between) 
+;; - lookup the docs on a command via w3 (place point on command and type
+;;   "C-c C-h")
 ;;
 ;; Please send bug-reports or feature suggestions to hackATrobfDOTde.
 
 ;;; Bugs:
 ;;
-;; - completions is not context aware
-;; - there should be a way to lookup the web docs from within Emacs 
-;; - multi-line comments are not fontified on the fly 
+;; - completions is not really context aware
+;; - multi-line comments are not fontified correctly while editing,
+;;   but only when fontifying the whole buffer.  If you know how to
+;;   fix this, please let me know!
 
 ;;; History:
 ;;
@@ -65,6 +64,7 @@
 
 (eval-when-compile
   (require 'font-lock)
+  (require 'w3)
   (require 'cl))
 
 ;;; Code:
@@ -73,8 +73,8 @@
   :group 'languages
   :prefix "ahk-")
 
-(defcustom ahk-mode-hook nil
-  "Hook run by `ahk-mode'."
+(defcustom ahk-mode-hook '(ahk-mode-hook-activate-filling)
+  "Hook functions run by `ahk-mode'."
   :type 'hook
   :group 'ahk-mode)
 
@@ -104,15 +104,16 @@
     (modify-syntax-entry ?]  "w" table)
     ;; some additional characters used in paths and switches  
     (modify-syntax-entry ?\\  "w" table)
-    (modify-syntax-entry ?/  "w" table)
+;    (modify-syntax-entry ?/  "w" table)
     (modify-syntax-entry ?-  "w" table)
     (modify-syntax-entry ?:  "w" table)
     (modify-syntax-entry ?.  "w" table)
     ;; for multiline comments (taken from cc-mode)
+    (modify-syntax-entry ?/  ". 14" table)
     (modify-syntax-entry ?*  ". 23"   table)
     ;; Give CR the same syntax as newline, for selective-display
-;    (modify-syntax-entry ?\^m "> b" table)
-;    (modify-syntax-entry ?\n "> b"  table)
+    (modify-syntax-entry ?\^m "> b" table)
+    (modify-syntax-entry ?\n "> b"  table)
     table)
   "Syntax table used in `ahk-mode' buffers.")
 
@@ -123,7 +124,8 @@
 
 (defvar ahk-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "\C-c\C-c" 'comment-region)
+    (define-key map "\C-c\C-h" 'ahk-www-help-at-point)
+    (define-key map "\C-c\C-c" 'ahk-comment-region)
     (define-key map "\C-c\C-i" 'ahk-insert-command-template)
     (define-key map "\t" 'ahk-indent-line-and-complete)
     (define-key map "{" 'ahk-electric-brace)
@@ -156,9 +158,11 @@ Will be initialized by `ahk-init'")
   "A list of all symbols available for completion
 Will be initialized by `ahk-init'")
 
-
-;(easy-menu-define ahk-menu ahk-mode-map "AHK Mode Commands"
-;		  (cons "AHK" ("0.1" ahk-mode-menu ahk)))
+(easy-menu-define ahk-menu ahk-mode-map "AHK Mode Commands"
+		  '("AHK"
+                    ["Insert Command Template" ahk-insert-command-template]
+                    ["Lookup webdocs on command" ahk-www-help-at-point]
+                    ))
 
 (defun ahk-init ()
   "Initialize ahk-mode variables.
@@ -185,7 +189,7 @@ This directory must be specified in the variable `ahk-syntax-directory'."
     (setq ahk-Commands-list nil)
     (goto-char 0)
     (while (not (eobp))
-      (if (not (looking-at "\\([^;\r\n][^\t\r\n, ]+\\)\\([^\r\n]*\\)"))
+      (if (not (looking-at "\\([^;\r\n][^(\t\r\n, ]+\\)\\([^\r\n]*\\)"))
           nil;; (error "Unknown file syntax")
         (setq ahk-Commands-list (cons (list
                                        (match-string 1)
@@ -241,6 +245,7 @@ This directory must be specified in the variable `ahk-syntax-directory'."
            '("\\s-*;.*$" .
              font-lock-comment-face)
            '("^/\\*\\(.*\r?\n\\)*\\(\\*/\\)?" .
+;           '(ahk-fontify-comment .
              font-lock-comment-face)
            '("^\\([^ \t\n:]+\\):" .
              (1 font-lock-builtin-face))
@@ -274,11 +279,50 @@ This directory must be specified in the variable `ahk-syntax-directory'."
   
   (message "Initializing ahk-mode variables done."))
 
+;; this was an attempt to get multi-line comments correctly highlighted, I
+;; tried to understand how cc-mode is doing it, but I have to admit I do not
+;; understand it!
+(defun ahk-fontify-comment (limit)
+;  (setq limit (point-max))
+  (save-excursion
+    (let (start end)
+;      (setq bstart (save-excursion
+;                     (re-search-backward "^/\\*" (point-min) t)))
+;      (setq bend (save-excursion
+;                   (re-search-backward "^\\*/" (point-min) t)))
+;      (if (and bstart bend (< bend bstart))
+;          (goto-char bstart))
+      (setq start (save-excursion
+                    (re-search-forward "^/\\*" limit t)))
+      (setq end (save-excursion
+                  (re-search-forward "^\\*/" limit t)))
+      
+      (cond
+       ((and start end (< end start))
+        (save-excursion
+          (re-search-forward "^\\*/" limit t)
+          t))
+       ((and start end (< start end))
+        (save-excursion
+          (re-search-forward "^/\\*\\(.*\r?\n\\)*\\*/" limit t)
+          t))
+       ((and start (not end))
+        (save-excursion
+          (re-search-forward "^/\\*\\(.*\r?\n\\)*" limit t)
+          t))
+       ))))
+
+(defun ahk-mode-hook-activate-filling ()
+  "Activates `auto-fill-mode' and `filladapt-mode'."
+  (auto-fill-mode 1)
+  (if (locate-library "filladapt")
+      (filladapt-mode 1)))
+
 ;;;###autoload
 (defun ahk-mode ()
   "Major mode for editing AutoHotKey Scripts.
 
-The hook `ahk-mode-hook' is run at mode initialization.
+The hook functions in `ahk-mode-hook' are run after mode initialization.
 
 Key bindings:
 \\{ahk-mode-map}"
@@ -300,7 +344,7 @@ Key bindings:
     (setq font-lock-keywords-case-fold-search t))
   
   (use-local-map ahk-mode-map)
-;  (easy-menu-add ahk-menu)
+  (easy-menu-add ahk-menu)
   (setq comment-start ";")
   (font-lock-mode 1)
   (force-mode-line-update)
@@ -315,25 +359,36 @@ Key bindings:
     ;; do a backward search to determin the indention level
     (save-excursion
       (beginning-of-line)
-      (skip-chars-backward " \t\n")
-      (beginning-of-line)
-      (if (looking-at "^[^: ]+:")
-          (if (looking-at "^[^: ]+:\\([^:]*:\\)?[ \t]*$")
-              (setq indent ahk-indetion)
-            (setq indent 0))
-        (if (looking-at "^\\([ \t]*\\){")
-            (setq indent (+ (length (match-string 1)) ahk-indetion))
-          (if (looking-at "^\\([ \t]*\\)")
-              (setq indent (+ (length (match-string 1))))))))
+      (if (looking-at "^;")
+          (setq indent 0)
+        (skip-chars-backward " \t\n")
+        (beginning-of-line)
+        (while (and (looking-at "^;") (not (bobp)))
+          (forward-line -1))
+        (if (looking-at "^[^: ]+:")
+            (if (looking-at "^[^: ]+:\\([^:]*:\\)?[ \t]*$")
+                (setq indent ahk-indetion)
+              (setq indent 0))
+          (if (looking-at "^\\([ \t]*\\)[{(]")
+              (setq indent (+ (length (match-string 1)) ahk-indetion))
+            (if (and
+                 (save-excursion
+                   (forward-line 1)
+                   (not (looking-at "^\\([ \t]*\\)[{(]")))
+                 (looking-at "^\\([ \t]*\\)\\(If\\|Else\\)"))
+                (setq indent (+ (length (match-string 1)) ahk-indetion))
+              (if (save-excursion
+                    (forward-line -1)
+                    (looking-at "^\\([ \t]*\\)\\(If\\|Else\\)"))
+                  (setq indent (+ (length (match-string 1))))
+                (if (looking-at "^\\([ \t]*\\)")
+                    (setq indent (+ (length (match-string 1)))))))))))
     ;; check for special tokens
     (save-excursion
       (beginning-of-line)
-      (if (looking-at "^\\([ \t]*\\)\\}")
+      (if (looking-at "^\\([ \t]*\\)[})]")
           (setq indent (- indent ahk-indetion))
         (if (or (looking-at "^[ \t]*[^,: \t\n]*:")
-                (and (looking-at "^\\([ \t]*\\)\\(Return\\|Exit\\)")
-                     (or (<= (length (match-string 1)) ahk-indetion)
-                         (= indent ahk-indetion)))
                 (looking-at "^;;;"))
             (setq indent 0))))
     (let ((p (point-marker)))
@@ -460,6 +515,47 @@ Key bindings:
     ;; jump to first parameter 
     (re-search-forward "\\<" end nil)
     (set-marker end nil)))
+
+(defun ahk-comment-region (start end &optional arg)
+  "Comment or uncomment each line in the region from START to END.
+If no region is active use the current line."
+  (interactive (if (region-active-p)
+                   (list (region-beginning)
+                         (region-end)
+                         current-prefix-arg)
+                 (let (start end)
+                   (beginning-of-line)
+                   (setq start (point))
+                   (forward-line)
+                   (setq end (point))
+                   (list start end current-prefix-arg))))
+  (save-excursion
+    (comment-region start end arg)))
+
+(defun ahk-www-help-at-point ()
+  (interactive)
+  (save-excursion
+    (re-search-backward "\\<\\w+")
+    (if (looking-at "\\<\\w+")
+        (ahk-www-help (match-string 0)))))
+
+(defvar ahk-www-help-alist '()
+  "Mapping of command to regexp for commands which are not unique.")
+
+(defun ahk-www-help (command)
+  "Display online help for the given command"
+  (interactive (list
+                (completing-read "AHK command: "
+                                 ahk-completion-list nil t)))
+  (w3-fetch "http://www.autohotkey.com/docs/commands.htm")
+  (goto-char (point-min))
+  (when (re-search-forward (concat "^|" (regexp-quote command)))
+    (goto-char (+ (match-beginning 0) 1))
+    (save-excursion
+      (if (re-search-forward (concat "^|" (regexp-quote command))
+                             (point-max) t)
+          (message "There is more than one occurrence of %s. Stopping at first match" command)
+        (widget-button-press (point))))))
 
 (provide 'ahk-mode)
 
